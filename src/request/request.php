c6e7390b6cad977e54c88137b14da471c05e8264
boyxp <isme@live.com>
2016-04-02 23:09:34 +0800

    新增request接口

---
+++ src/request/request.php
@@ -0,0 +1,22 @@
+<?php
+namespace bluefin\http\request;
+interface request
+{
+	public function getMethod():string;
+	public function getUri():string;
+	public function getScheme():string;
+	public function getHost():string;
+	public function getPort():string;
+	public function getPath():string;
+	public function getBasename():string;
+	public function getFilename():string;
+	public function getExtension():string;
+	public function getQueryString():string;
+	public function getQuery(string $key):string;
+	public function getClientIp():string;
+	public function getUser():string;
+	public function getPassword():string;
+	public function getReferer():string;
+	public function getUserAgent():string;
+	public function __get(string $key):string;
+}
